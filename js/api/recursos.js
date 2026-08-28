8fb01cd29f9df379780d55c7e68e4b567a3e2287
Abel <abelvelazco077@gmail.com>
2026-08-28 16:34:21 -0300

    correccion de recursos.js

--- js/api/recursos.js
+++ js/api/recursos.js
@@ -1,6 +1,9 @@
 import { pedirDatos } from "./client.js";
+
+//URL base reutilizable
 export const URL_BASE = "https://www.thesportsdb.com/api/v1/json/123/";
 
+//Array de las ligas que va a esperar nuestra API
 const LIGAS = [
     "English Premier League",
     "Spanish La Liga",
@@ -8,13 +11,27 @@ const LIGAS = [
     "German Bundesliga",
     "French Ligue 1"
 ];
+
+// Async Function que trae los equipos de las 5 grandes ligas europeas
+// combinados en un solo array
 export async function topEquiposEuropa() {
-    const promesas = LIGAS.map
-    (liga => pedirDatos(`${URL_BASE}search_all_teams.php?l=${encodeURIComponent(liga)}`));
+    // uso .map para hacer las 5 llamadas en paralelo, la constante
+    // promesas termina siendo un array de 5 promesas pendientes
+    const promesas = LIGAS.map (liga => 
+        pedirDatos(
+            `${URL_BASE}search_all_teams.php?l=${encodeURIComponent(liga)}`
+        )
+    );
+    
+    // uso .allSettled por si alguna llamada falla, asi no se abortan las demas
     const resultados = await Promise.allSettled(promesas);
 
     const equipos = resultados
+        // Filtramos solo las ligas que respondieron bien
         .filter(r => r.status === "fulfilled")
+        // usamos .flatMap para aplanar los 5 arrays de
+        // equipos en uno solo
         .flatMap(r => r.value.teams ?? []);
+
     return equipos;
 }
